e7f5c46731e2cae169cd34d8a480ef9ec156f921
lauren <ljdutton2@icloud.com>
2021-02-03 11:52:16 -0900

    read write render

--- makesite.go
+++ makesite.go
@@ -1,6 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"html/template"
+)
+
+
+type Content struct {
+Content string
+}
+
+func readFile(name string) string {
+	fileContents, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(fileContents)
+}
+
+func renderTemplate(filename string, data string) {
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+	content := Content{Content: data}
+	err := t.Execute(os.Stdout, content)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writeFile(file string, data string) {
+	bytesToWrite := []byte(data)
+	err := ioutil.WriteFile(file, bytesToWrite, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fmt.Println("Hello, world!")
